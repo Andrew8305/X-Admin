1cee6a802c169cc7f6e3118b547ca401b442df3e
DESKTOP-A9HEVQA\leaf <806569552@qq.com>
2018-02-24 21:14:20 +0800

    添加自定义日期序列化类

---
+++ src/main/java/com/leaf/xadmin/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,24 @@
+package com.leaf.xadmin.utils.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * 毫秒日期转换为秒日期
+ *
+ * @author leaf
+ * <p>date: 2018-02-24 12:10</p>
+ * <p>version: 1.0</p>
+ */
+public class Date2LongSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date date, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException, JsonProcessingException {
+        jsonGenerator.writeNumber(date.getTime() / 1000);
+    }
+}
