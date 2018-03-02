96352896a2eec495773b3b0229827366827b57ef
DESKTOP-A9HEVQA\leaf <806569552@qq.com>
2018-03-02 16:48:57 +0800

    添加多数据源支持，暂测试失败

---
+++ src/main/java/com/leaf/xadmin/config/datasource/DataSourceContextHolder.java
@@ -0,0 +1,37 @@
+package com.leaf.xadmin.config.datasource;
+
+/**
+ * 上下文数据源
+ *
+ * @author leaf
+ * <p>date: 2018-02-28 12:35</p>
+ * <p>version: 1.0</p>
+ */
+public class DataSourceContextHolder {
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+
+    /**
+     * 设置数据源
+     *
+     * @param dataSourceTypeEnum
+     */
+    public static void setDataSourceType(String dataSourceTypeEnum) {
+        CONTEXT_HOLDER.set(dataSourceTypeEnum);
+    }
+
+    /**
+     * 取得当前数据源
+     *
+     * @return
+     */
+    public static String getDataSourceType() {
+        return CONTEXT_HOLDER.get();
+    }
+
+    /**
+     * 清除上下文数据源
+     */
+    public static void clearDataSourceType() {
+        CONTEXT_HOLDER.remove();
+    }
+}
---
+++ src/main/java/com/leaf/xadmin/config/datasource/DataSourceTypeEnum.java
@@ -0,0 +1,18 @@
+package com.leaf.xadmin.config.datasource;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ *
+ * @author leaf
+ * <p>date: 2018-02-28 12:35</p>
+ * <p>version: 1.0</p>
+ */
+@Getter
+@AllArgsConstructor
+public enum DataSourceTypeEnum {
+    // 数据源枚举
+    PRIMARY("primary"), SECOND("second");
+    private String value;
+}
