767e00138dd65dfac7fa2f32bd2c91feb3355b91
leaf <806569552@qq.com>
2018-03-05 19:47:19 +0800

    采用混合数据源，来实现多个数据源的共同使用

--- src/main/java/com/leaf/xadmin/config/MybatisPlusConfig.java
+++ src/main/java/com/leaf/xadmin/config/MybatisPlusConfig.java
@@ -1,7 +1,21 @@
 package com.leaf.xadmin.config;
 
-import org.mybatis.spring.annotation.MapperScan;
+import com.baomidou.mybatisplus.MybatisConfiguration;
+import com.baomidou.mybatisplus.MybatisXMLLanguageDriver;
+import com.baomidou.mybatisplus.entity.GlobalConfiguration;
+import com.baomidou.mybatisplus.mapper.LogicSqlInjector;
+import com.baomidou.mybatisplus.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
+import com.baomidou.mybatisplus.plugins.SqlExplainInterceptor;
+import com.baomidou.mybatisplus.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 /**
  * mybatis-plus配置
@@ -10,7 +24,42 @@ import org.springframework.context.annotation.Configuration;
  * <p>date: 2017-12-31 1:11</p>
  */
 @Configuration
-@MapperScan(basePackages = "com.leaf.xadmin.mapper")
 public class MybatisPlusConfig {
+
+    /**
+     * 创建sqlSessionFactory
+     *
+     * @param dataSource
+     * @return
+     * @throws Exception
+     */
+    public static SqlSessionFactory createSqlSessionFactory(DataSource dataSource) throws Exception {
+        MybatisSqlSessionFactoryBean sqlSessionFactory = new MybatisSqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setTypeAliasesPackage("com.leaf.xadmin.entity, com.leaf.xadmin.constants");
+        sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/*.xml"));
+        sqlSessionFactory.setTypeEnumsPackage("com.leaf.xadmin.vo.enums");
+        MybatisConfiguration configuration = new MybatisConfiguration();
+        configuration.setDefaultScriptingLanguage(MybatisXMLLanguageDriver.class);
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setCacheEnabled(false);
+        configuration.setJdbcTypeForNull(JdbcType.NULL);
+        sqlSessionFactory.setConfiguration(configuration);
+        sqlSessionFactory.setPlugins(new Interceptor[]{
+                new PaginationInterceptor(),
+                new PerformanceInterceptor(),
+                new OptimisticLockerInterceptor(),
+                new SqlExplainInterceptor()
+        });
+        GlobalConfiguration globalConfiguration = new GlobalConfiguration(new LogicSqlInjector());
+        globalConfiguration.setIdType(2);
+        globalConfiguration.setFieldStrategy(2);
+        globalConfiguration.setDbColumnUnderline(true);
+        globalConfiguration.setRefresh(true);
+        globalConfiguration.setLogicDeleteValue("0");
+        globalConfiguration.setLogicNotDeleteValue("1");
+        sqlSessionFactory.setGlobalConfig(globalConfiguration);
+        return sqlSessionFactory.getObject();
+    }
 
 }
---
+++ src/main/java/com/leaf/xadmin/config/datasource/PrimaryDatabaseConfig.java
@@ -0,0 +1,56 @@
+package com.leaf.xadmin.config.datasource;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.leaf.xadmin.config.MybatisPlusConfig;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author leaf
+ * <p>date: 2018-03-05 18:24</p>
+ * <p>version: 1.0</p>
+ */
+@Configuration
+@EnableTransactionManagement
+@PropertySource(value = {"classpath:druid/druid.properties"})
+@MapperScan(basePackages = "com.leaf.xadmin.mapper.primary", sqlSessionTemplateRef = "primarySqlSessionTemplate")
+public class PrimaryDatabaseConfig {
+
+    @Primary
+    @Bean(name = "primaryDataSource")
+    @ConfigurationProperties(prefix = "primary")
+    public DruidDataSource customDataSource() {
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Primary
+    @Bean
+    public PlatformTransactionManager customTransactionManager(@Qualifier("primaryDataSource")DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Primary
+    @Bean(name = "primarySqlSessionTemplate")
+    public SqlSessionTemplate customSqlSessionTemplate(@Qualifier("primarySqlSessionFactory") SqlSessionFactory sqlSessionFactory) throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Primary
+    @Bean(value = "primarySqlSessionFactory")
+    public SqlSessionFactory customSqlSessionFactory(@Qualifier(value = "primaryDataSource") DataSource dataSource) throws Exception {
+        return MybatisPlusConfig.createSqlSessionFactory(dataSource);
+    }
+}
